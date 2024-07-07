39a1363b0b266132943e10c4ada5e400ca4da112
Shouvik <shouvik.islam.06@gmail.com>
2024-07-07 19:42:04 +0600

    Add files via upload

---
+++ views/admin/category/index.blade.php
@@ -0,0 +1,65 @@
+@extends('admin.master')
+
+
+@section('title')
+    Category
+@endsection
+
+@section('body')
+    <div class="page-header">
+        <div>
+            <h1 class="page-title">Manage Category</h1>
+        </div>
+        <div class="ms-auto pageheader-btn">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="javascript:void(0);">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+            </ol>
+        </div>
+    </div>
+
+    <div class="row row-sm">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header border-bottom">
+                    <h3 class="card-title">All Category Information</h3>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered text-nowrap border-bottom" id="basic-datatable">
+                            <thead>
+                            <tr>
+                                <th class="wd-15p border-bottom-0">SL. No</th>
+                                <th class="wd-15p border-bottom-0">Name</th>
+                                <th class="wd-20p border-bottom-0">Description</th>
+                                <th class="wd-15p border-bottom-0">Image</th>
+                                <th class="wd-10p border-bottom-0">Status</th>
+                                <th class="wd-25p border-bottom-0">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($categories as $category)
+                                <tr>
+                                    <td>{{$category->id}}</td>
+                                    <td>{{$category->name}}</td>
+                                    <td>{{$category->description}}</td>
+                                    <td><img src="{{asset('/')}}{{$category->image}}" alt="image" width="140px"></td>
+                                    <td>{{$category->status}}</td>
+                                    <td>
+                                        <a href="" class="btn btn-success btn-sm">
+                                            <i class="fa fa-edit"></i>
+                                        </a>
+                                        <a href="" class="btn btn-danger btn-sm">
+                                            <i class="fa fa-trash"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
