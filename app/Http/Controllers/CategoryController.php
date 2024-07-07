f43b3521cdf6fd78a6e1be9c5202b8d1ffc456b8
Shouvik <shouvik.islam.06@gmail.com>
2024-07-07 21:00:44 +0600

    Add files via upload

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -19,7 +19,25 @@ class CategoryController extends Controller
     }
 
     public function store(Request $request) {
+//        $formFields = $request->validate([
+//            'name' => 'required|unique',
+//        ]);
+
         Category::newCategory($request);
-        return back()->with('message', 'category created successfully');
+        return back()->with('message', 'Category created successfully.');
+    }
+
+    public function edit($id) {
+        return view('admin.category.edit', ['category' => Category::find($id)]);
+    }
+
+    public function update(Request $request, $id) {
+        Category::updateCategory($request, $id);
+        return redirect('/category')->with('message', 'Category info update successfully.');
+    }
+
+    public function destroy($id) {
+        Category::deleteCategory($id);
+        return back()->with('message', 'Category deleted successfully.');
     }
 }
